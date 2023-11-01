b82ea2f6e04ceefa59e95dc090337b3c7f695d99
MbahDaniel <mbahdaniel58@gmail.com>
2023-11-01 13:05:33 -0700

    deleted a file::

--- hello.py
+++
@@ -1,6 +0,0 @@
-print("This is a simple program")
-print() #Output a blank  line to help with layout
-name = input("Please input your name:")#ask for the input
-print ("Hello",name) #joins "Hello" and their name together
-"Daniel" == "name" ("what a coincidence daniel:")#same name 
-    
