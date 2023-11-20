8fd32749bb194c742842a07bcd758fc491043d25
MbahDaniel <mbahdaniel58@gmail.com>
2023-11-20 15:05:57 -0800

    Newpush

--- chalenge_13.py
+++ chalenge_13.py
@@ -7,4 +7,23 @@ file.write("12, ")
 file.close()
 
 #question 2
-file.open("names.txt", "w")
+file = open("names.txt", "w")
+file.write("david\n")
+file.write("daniel\n")
+file.write("efosa\n")
+file.write("victor\n")
+file.write("nuel\n")
+file.close()
+
+#question 3
+file = open("Names.txt","r")
+print(file.red())
+file.close()
+
+#question 4
+file=open("name.txt","w")
+new_name = input("enter a new name: ")
+file.write(f"{new_name} \n")
+
+#question 5
+file = open("names.txt")
